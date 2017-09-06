a7992ef53c748590426d4f908f5839e7fbac6fb8
Anderson Felisberto Manoel <andynhofm@gmail.com>
2017-09-05 23:22:10 -0300

    Exemplo de uso da câmera naiva do ionic

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {Camera, CameraOptions} from "@ionic-native/camera";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  public imagemBase64: string;
+  private _opcoes: CameraOptions = {
+    quality: 100,
+    mediaType: this._camera.MediaType.PICTURE,
+    destinationType: this._camera.DestinationType.DATA_URL,
+    encodingType: this._camera.EncodingType.JPEG,
+    correctOrientation: true
+  };
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController,
+              private _camera: Camera) {
 
+  }
+
+  inicializaCamera() {
+    this._camera.getPicture(this._opcoes).then(imagem => {
+      this.imagemBase64 = "data:image/jpeg;base64," + imagem;
+    }).catch(err => {
+      console.log(err);
+      this.imagemBase64 = "";
+    })
   }
 
 }
